4e1bc43017f98ab31ada55c9e456dbedf0aeaf97
Cinnamals <mattiasreuterskiold@gmail.com>
2024-08-22 14:50:14 +0200

    theme independant of machine

--- settings/theme-init.el
+++ settings/theme-init.el
@@ -1,19 +1,5 @@
 (load-theme 'tango-dark t)
 
-(let ((h (system-name)))
-  (cond ((at-home-p)
-         (set-face-attribute 'default nil
-                             :family "Iosevka"
-                             :height 110
-                             :weight 'normal
-                             :width 'normal))
-         ((at-work-p)
-          (set-face-attribute 'default nil
-                              :family "JetBrains Mono"
-                              :height 90
-                              :weight 'normal
-                              :width 'normal))))
-
 (setq frame-title-format `("%b@emacs" ,(number-to-string emacs-major-version)))
 
 (provide 'theme-init)
