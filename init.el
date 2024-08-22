02e933c7952a1fc59412614491747af6c69b07d9
Cinnamals <mattiasreuterskiold@gmail.com>
2024-08-22 14:36:19 +0200

    removed .ini settings

--- init.el
+++ init.el
@@ -44,15 +44,6 @@
 (add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
 (package-initialize)
 
-;;; TODO: `use-package' is included in Emacs 29. Once Emacs 29 is generally
-;;; available, this can be removed.
-(unless (package-installed-p 'use-package)
-  (package-refresh-contents)
-  (package-install 'use-package))
-
-(eval-when-compile
-  (require 'use-package))
-
 (require 'bind-key)
 
 (require 'theme-init)
@@ -80,32 +71,11 @@
 (require 'avy-init)
 (require 'languagetool-init nil 'noerror)
 (require 'treesit-init)
-
-;; Load API keys from secret.el if present on the system.
-(require 'secret nil 'noerror)
-
 (require 'key-bindings)
-
-;; Scrolling on some mice results in a 'C-M-[()]'
-;; bind these to scroll.
-(global-set-key (kbd "C-M-(") 'mwheel-scroll)
-(global-set-key (kbd "C-M-)") 'mwheel-scroll)
 
 (custom-set-variables
  '(package-selected-packages
-   '(ox-slack vue-mode ox-jira puppet-mode python-black pip-requirements speed-type dired-narrow editorconfig po-mode python-pytest ayu-theme spacemacs-theme github-theme string-inflection fill-column-indicator htmlize which-key expand-region org-bullets multiple-cursors matlab-mode minions moody doom-modeline poet-theme highlight-escape-sequences gruvbox-theme hl-todo highlight2clipboard dashboard all-the-icons counsel-tramp yasnippet counsel-spotify ivy-rich gist glsl-mode company-anaconda anaconda-mode kotlin-mode flycheck-kotlin cmake-font-lock rjsx-mode dap-mode projectile-ripgrep flycheck flycheck-mode lsp-ui cquery company-lsp use-package swiper company))
- '(safe-local-variable-values
-   '((eval when
-           (and
-            (not
-             (eq major-mode 'conf-windows-mode))
-            (buffer-file-name)
-            (file-regular-p
-             (buffer-file-name))
-            (string-match-p "^[^.].*\\.ini"
-                            (file-name-nondirectory
-                             (buffer-file-name))))
-           (conf-windows-mode)))))
+   '(ox-slack vue-mode ox-jira puppet-mode python-black pip-requirements speed-type dired-narrow editorconfig po-mode python-pytest ayu-theme spacemacs-theme github-theme string-inflection fill-column-indicator htmlize which-key expand-region org-bullets multiple-cursors matlab-mode minions moody doom-modeline poet-theme highlight-escape-sequences gruvbox-theme hl-todo highlight2clipboard dashboard all-the-icons counsel-tramp yasnippet counsel-spotify ivy-rich gist glsl-mode company-anaconda anaconda-mode kotlin-mode flycheck-kotlin cmake-font-lock rjsx-mode dap-mode projectile-ripgrep flycheck flycheck-mode lsp-ui cquery company-lsp use-package swiper company)))
 
 (put 'narrow-to-region 'disabled nil)
 (put 'upcase-region 'disabled nil)
