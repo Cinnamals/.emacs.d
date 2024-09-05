2ea28655d964ee69497a7300aac051b1462281a8
Cinnamals <mattiasreuterskiold@gmail.com>
2024-09-06 00:59:11 +0200

    minor clean up, tiny bit more modular paths lol

--- settings/dart-init.el
+++ settings/dart-init.el
@@ -1,10 +1,14 @@
-(setq package-selected-packages
-      '(dart-mode lsp-mode lsp-dart lsp-treemacs flycheck company
-                  lsp-ui company hover))
+(defvar my-flutter-sdk-path (expand-file-name "~/dev-sdk/flutter")
+  "Path to the Flutter SDK bin")
+
+(defvar my-dart-sdk-dir (expand-file-name "bin/cache/dart-sdk/" my-flutter-sdk-path)
+  "Path to the Dart SDK bin (inside Flutter SDK).")
+
+(add-to-list 'load-path (expand-file-name "manually-installed-packages" user-emacs-directory))
+
+(add-to-list 'auto-mode-alist '("\\.dart\\'" . dart-mode))
 
 (add-hook 'dart-mode-hook 'lsp)
-(add-to-list 'load-path "~/.emacs.d/manually-installed-packages/")
-(add-to-list 'auto-mode-alist '("\\.dart\\'" . dart-mode))
 
 (use-package dart-mode
   :hook (dart-mode . flutter-test-mode))
@@ -14,13 +18,13 @@
   :bind (:map dart-mode-map
               ("C-M-x" . #'flutter-run-or-hot-reload))
   :config
-  (setq flutter-sdk-path (expand-file-name "~/dev-sdk/flutter")))
+  (setq flutter-sdk-path my-flutter-sdk-path ))
 
 (use-package lsp-mode
   :ensure t
   :hook (dart-mode . lsp)
   :commands lsp
   :config
-  (setq lsp-dart-sdk-dir (expand-file-name "~/dev-sdk/flutter/bin/cache/dart-sdk")))
+  (setq lsp-dart-sdk-dir my-dart-sdk-dir ))
 
 (provide 'dart-init)
