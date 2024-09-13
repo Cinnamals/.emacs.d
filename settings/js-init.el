432a1bf123996a3a4938f70230930cd773776d46
Cinnamals <mattiasreuterskiold@gmail.com>
2024-09-13 23:19:39 +0200

    Seperate ts/js mode. Add js-doc

--- settings/js-init.el
+++ settings/js-init.el
@@ -4,20 +4,46 @@
 	      ("C-M-k" . c-doc-comment)
 	      ("C-M-j" . c-block-comment)))
 
-(defun my/ts-hook ()
+(use-package js-doc
+  :ensure t)
+
+;; ;; ;; TS mode
+;; (defun my/ts-hook ()
+;;   (interactive)
+;;   (lsp-mode)
+;;   (flycheck-mode 1)
+;;   (setq flycheck-check-syntax-automatically '(save mode-enabled))
+
+;;   (eldoc-mode 1)
+;;   (company-mode 1)
+;;   (js-doc-mode 1)
+;;   (define-key typescript-mode-map (kbd "C-c i") 'js-doc-insert-function-doc))
+
+;; (use-package typescript-mode
+;;   :ensure t
+;;   :mode (("\\.ts\\'" . typescript-mode)
+;;          ("\\.js\\'" . typescript-mode)
+;;          ("\\.mjs\\'" . typescript-mode))
+;;   :hook (typescript-mode . my/ts-hook))
+;; ;; ;; TS mode
+
+
+;; ;; JS mode
+(defun my/js-hook ()
   (interactive)
-  (lsp-mode)
+  (lsp)
+  (company-mode 1)
+  (eldoc-mode 1)
   (flycheck-mode 1)
   (setq flycheck-check-syntax-automatically '(save mode-enabled))
 
-  (eldoc-mode 1)
-  (company-mode 1))
+  (define-key js-mode-map (kbd "C-c i") 'js-doc-insert-function-doc))
 
-(use-package typescript-mode
-  :ensure t
-  :mode (("\\.ts\\'" . typescript-mode)
-         ("\\.js\\'" . typescript-mode)
-         ("\\.mjs\\'" . typescript-mode))
-  :hook (typescript-mode . my/ts-hook))
+(use-package js-mode
+  :ensure nil
+  :mode (("\\.js\\'" . js-mode)
+         ("\\.mjs\\'" . js-mode))
+  :hook (js-mode . my/js-hook))
+;; ;; JS MODE
 
 (provide 'js-init)
