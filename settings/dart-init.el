1435bc9686eb5bc467653970eccb2ad2b8e08483
Cinnamals <mattiasreuterskiold@gmail.com>
2024-09-15 12:30:57 +0200

    Updated dart-mode init

--- settings/dart-init.el
+++ settings/dart-init.el
@@ -4,47 +4,22 @@
 (defvar my-dart-sdk-dir (expand-file-name "bin/cache/dart-sdk/" my-flutter-sdk-path)
   "Path to the Dart SDK bin (inside Flutter SDK).")
 
+(setq package-selected-packages
+      '(dart-mode lsp-dart lsp-treemacs))
+
 (add-to-list 'load-path (expand-file-name "manually-installed-packages" user-emacs-directory))
-
-(add-to-list 'auto-mode-alist '("\\.dart\\'" . dart-mode))
-
-(add-hook 'dart-mode-hook 'lsp)
-
-(use-package dart-mode
-  :hook (dart-mode . flutter-test-mode))
 
 (use-package flutter
   :after dart-mode
   :bind (:map dart-mode-map
               ("C-M-x" . #'flutter-run-or-hot-reload))
   :config
-  (setq flutter-sdk-path my-flutter-sdk-path ))
-
-(use-package lsp-mode
-  :ensure t
-  :hook (dart-mode . lsp)
-  :commands lsp
-  :config
   (setq lsp-dart-sdk-dir my-dart-sdk-dir ))
 
-(defun my-dart-mode-setup ()
-  (setq-local company-idle-delay 0.1)
-  (setq-local flycheck-checker 'lsp)
-  (setq-local flycheck-idle-change-delay 0.2)
-  (setq-local lsp-file-watch-threshold 200)
-  (setq-local lsp-diagnostics-max-number 5)
-  (setq-local lsp-idle-delay 1.0)
-  (setq-local lsp-file-watch-ignored
-        '("[/\\\\]\\.git$" "[/\\\\]node_modules$" "[/\\\\]build$" "[/\\\\]dist$"))
-  (add-hook 'post-self-insert-hook 'my-company-complete-after-dot nil t))
+(when (cl-find-if-not #'package-installed-p package-selected-packages)
+  (package-refresh-contents)
+  (mapc #'package-install package-selected-packages))
 
-(add-hook 'dart-mode-hook 'my-dart-mode-setup)
-
-;; Ensure that the worlds most annoying auto-complete hook only triggers in dart-mode
-(defun my-company-complete-after-dot ()
-  (when (and (derived-mode-p 'dart-mode) (looking-back "\\." 1))
-    (company-complete)))
-
-(add-hook 'post-self-insert-hook 'my-company-complete-after-dot)
+(add-hook 'dart-mode-hook 'lsp)
 
 (provide 'dart-init)
