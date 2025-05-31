e0cac6b21e8bc29ff992d8c388436e7d60f329da
Cinnamals <mattiasreuterskiold@gmail.com>
2025-05-31 17:58:00 +0200

    Add unzip function for dired mode

--- settings/dired-init.el
+++ settings/dired-init.el
@@ -11,6 +11,20 @@
   (end-of-buffer)
   (dired-previous-line 1))
 
+
+(defun unzip-file-in-dired ()
+  "Unzip the .zip file at point in dired into a directory with the same name without .zip."
+  (interactive)
+  (let* ((file (dired-get-file-for-visit))
+         (ext (file-name-extension file))
+         (output-dir (file-name-sans-extension file)))
+    (if (and ext (string= ext "zip"))
+        (progn
+          (shell-command (format "unzip -d %s %s" (shell-quote-argument output-dir) (shell-quote-argument file)))
+          (message "Unzipped %s into %s" file output-dir)
+          (revert-buffer)) ;; refresh dired buffer
+      (message "Not a zip file: %s" file))))
+
 (defun my/dired-mode-hook ()
   (local-set-key (kbd "M-<") 'dired-beginning-of-buffer)
   (local-set-key (kbd "M->") 'dired-end-of-buffer)
